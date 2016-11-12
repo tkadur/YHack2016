868aefb19f53ad07ebcdce8647465b9ec0f419a1
tkadur <thejas.kadur@gmail.com>
2016-11-12 02:06:18 -0500

    Add gyro support and camera optimizations

--- script.js
+++ script.js
@@ -1,3 +1,97 @@
+THREE.DeviceOrientationControls = function ( object ) {
+ 
+        var scope = this;
+ 
+        this.object = object;
+        this.object.rotation.reorder( "YXZ" );
+ 
+        this.enabled = true;
+ 
+        this.deviceOrientation = {};
+        this.screenOrientation = 0;
+ 
+        var onDeviceOrientationChangeEvent = function ( event ) {
+ 
+                scope.deviceOrientation = event;
+ 
+        };
+ 
+        var onScreenOrientationChangeEvent = function () {
+ 
+                scope.screenOrientation = window.orientation || 0;
+ 
+        };
+ 
+        // The angles alpha, beta and gamma form a set of intrinsic Tait-Bryan angles of type Z-X'-Y''
+ 
+        var setObjectQuaternion = function () {
+ 
+                var zee = new THREE.Vector3( 0, 0, 1 );
+ 
+                var euler = new THREE.Euler();
+ 
+                var q0 = new THREE.Quaternion();
+ 
+                var q1 = new THREE.Quaternion( - Math.sqrt( 0.5 ), 0, 0, Math.sqrt( 0.5 ) ); // - PI/2 around the x-axis
+ 
+                return function ( quaternion, alpha, beta, gamma, orient ) {
+ 
+                        euler.set( beta, alpha, - gamma, 'YXZ' );                       // 'ZXY' for the device, but 'YXZ' for us
+ 
+                        quaternion.setFromEuler( euler );                               // orient the device
+ 
+                        quaternion.multiply( q1 );                                      // camera looks out the back of the device, not the top
+ 
+                        quaternion.multiply( q0.setFromAxisAngle( zee, - orient ) );    // adjust for screen orientation
+ 
+                }
+ 
+        }();
+ 
+        this.connect = function() {
+ 
+                onScreenOrientationChangeEvent(); // run once on load
+ 
+                window.addEventListener( 'orientationchange', onScreenOrientationChangeEvent, false );
+                window.addEventListener( 'deviceorientation', onDeviceOrientationChangeEvent, false );
+ 
+                scope.enabled = true;
+ 
+        };
+ 
+        this.disconnect = function() {
+ 
+                window.removeEventListener( 'orientationchange', onScreenOrientationChangeEvent, false );
+                window.removeEventListener( 'deviceorientation', onDeviceOrientationChangeEvent, false );
+ 
+                scope.enabled = false;
+ 
+        };
+ 
+        this.update = function () {
+ 
+                if ( scope.enabled === false ) return;
+ 
+                var alpha  = scope.deviceOrientation.alpha ? THREE.Math.degToRad( scope.deviceOrientation.alpha ) : 0; // Z
+                var beta   = scope.deviceOrientation.beta  ? THREE.Math.degToRad( scope.deviceOrientation.beta  ) : 0; // X'
+                var gamma  = scope.deviceOrientation.gamma ? THREE.Math.degToRad( scope.deviceOrientation.gamma ) : 0; // Y''
+                var orient = scope.screenOrientation       ? THREE.Math.degToRad( scope.screenOrientation       ) : 0; // O
+ 
+                setObjectQuaternion( scope.object.quaternion, alpha, beta, gamma, orient );
+ 
+        };
+ 
+        this.connect();
+ 
+};
+
+var gyroPresent = false;
+window.addEventListener("devicemotion", function(event){
+    if(event.rotationRate.alpha || event.rotationRate.beta || event.rotationRate.gamma)
+        gyroPresent = true;
+});
+
+
 var scene = new THREE.Scene();
 var consoleScene = new THREE.Scene();
 
@@ -17,12 +111,16 @@ var camera = new THREE.PerspectiveCamera(
 	window.innerWidth / window.innerHeight,
 	0.1,
 	500);
+
+var controls;
+controls = new THREE.DeviceOrientationControls( camera );
+/*
 var consoleCamera = new THREE.PerspectiveCamera(
 	75,
 	window.innerWidth / window.innerHeight,
 	0.1,
 	500);
-
+*/
 // http://stackoverflow.com/a/29269912/1517227
 var renderer = new THREE.WebGLRenderer({
 	antialias: true
@@ -44,7 +142,7 @@ var negativeX = 1;
 var positiveZ = 2;
 var positiveX = 3;
 
-var cameraDistance = 300;
+var cameraDistance = 0;
 var cameraDir = 0;
 var cameraYRotation = 0;
 var cameraPosition = new THREE.Vector3(0, 0, cameraDistance);
@@ -52,7 +150,7 @@ var rotationX = 0;
 var rotationY = 0;
 
 camera.position.z = cameraDistance;
-consoleCamera.position.z = cameraDistance;
+//consoleCamera.position.z = cameraDistance;
 
 var font;
 var fontLoader = new THREE.FontLoader();
@@ -126,10 +224,14 @@ var thing = function(type, position) {
 	this.positions = [];
 	this.colors = [];
 
-	this.numLettersRequired = 7500;
+	if (gyroPresent) {
+		this.numLettersRequired = 1500;
+	} else {
+		this.numLettersRequired = 3500;
+	}
 	this.isBlob = true;
 	
-	for (var x = 0; x < 7500; x++) {
+	for (var x = 0; x < this.numLettersRequired; x++) {
 		var vec = new THREE.Vector3(
 			Math.random() * 30 - 15,
 			Math.random() * 30 - 15,
@@ -154,7 +256,7 @@ thing.prototype.assignNewDestination = function() {
 
 	return new THREE.Vector3(
 		Math.random() * 1000 - 500,
-		Math.random() * 50 - 55,
+		Math.random() * 50 - 53,
 		Math.random() * 1000 - 500
 	);
 
@@ -181,7 +283,7 @@ var letter = function(type, character, font) {
 	this.position = new THREE.Vector3(0, 0, 0);
 	this.velocity = new THREE.Vector3(0, 0, 0);
 	this.destination = new THREE.Vector3(0, 0, 0);
-	this.speed = Math.random() * 1 + 2;
+	this.speed = Math.random() * 1 + 1.5;
 
 	this.isFree = false;
 	this.isDead = false;
@@ -420,17 +522,22 @@ function render() {
 	requestAnimationFrame(render);
 
 	renderer.clear();
+	/*
 	renderer.setViewport(0, 0, window.innerWidth, window.innerHeight);
 	renderer.render(consoleScene, consoleCamera);
 
 	renderer.clearDepth();
+	*/
 	renderer.setViewport(0, 0, window.innerWidth, window.innerHeight);
 	renderer.render(scene, camera);
 
 	tick++;
-
-	camera.rotation.x += (rotationX - camera.rotation.x) / 20;
-	camera.rotation.y += ((cameraYRotation + rotationY) - camera.rotation.y) / 20;
+	if (gyroPresent) {
+		controls.update();
+	} else {
+		camera.rotation.x += (rotationX - camera.rotation.x) / 20;
+		camera.rotation.y += ((cameraYRotation + rotationY) - camera.rotation.y) / 20;
+	}
 	
 	/*
 	for (var iter = 0; iter < 1; iter++) {
