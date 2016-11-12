bd2f17a14c9dbcff25cde627221b846fcddf3b39
tkadur <thejas.kadur@gmail.com>
2016-11-12 04:51:23 -0500

    what do the words mean

--- script.js
+++ script.js
@@ -91,6 +91,8 @@ window.addEventListener("devicemotion", function(event){
         gyroPresent = true;
 });
 
+var camVector = new THREE.Vector3();
+
 
 var scene = new THREE.Scene();
 var consoleScene = new THREE.Scene();
@@ -266,7 +268,7 @@ function makeThing(type, position) {
 	things.push(new thing(type, position));
 }
 
-makeThing(player_type, new THREE.Vector3(0, -100, 0));
+makeThing(player_type, new THREE.Vector3(0, 0, 0));
 
 var scene_type = 0;
 var console_type = 1;
@@ -311,6 +313,8 @@ var letter = function(type, character, font) {
 	this.sceneTickToForm = 100;
 	this.sceneArrived = false;
 
+	this.stuck = false;
+
 	
 	// snowflakes
 	//this.isSnowflake = false;
@@ -332,6 +336,14 @@ letter.prototype.setDestination = function(x, y, z) {
 }
 
 letter.prototype.update = function() {
+	/*
+	if (this.text == "A") {
+		//this.free();
+		this.setPosition(camera.position.x, 100, 100);
+		this.speed = 0;
+		return;
+	}
+	*/
 	if (this.isFree) {
 		this.material.opacity -= 0.01;
 		if (this.material.opacity < 0.05) {
@@ -376,7 +388,7 @@ letter.prototype.update = function() {
 			this.sceneTick++;
 
 			if (this.sceneTickToForm <= 0) {
-				this.mesh.rotation.y = this.sceneTick * this.randomFactor * 0.01;
+				this.mesh.rotation.y = Math.PI - this.sceneTick * this.randomFactor * 0.01;
 
 				if (this.mesh.position.distanceTo(this.destination) < 2) {
 					this.sceneArrived = true;
@@ -475,7 +487,7 @@ function init(font) {
 
 	// ask the player to make a move
 }
-
+/*
 function addConsoleString(s) {
 	for (var i = 0; i < s.length; i++) {
 		if (s[i] != "\n") {
@@ -515,10 +527,16 @@ function addConsoleString(s) {
 		}
 	}
 }
-
+*/
 var tick = 0;
 
+var targetStr = "";
+
 function render() {
+
+	var target = targetStr.split("");
+	var counter = 0;
+	
 	requestAnimationFrame(render);
 
 	renderer.clear();
@@ -539,11 +557,13 @@ function render() {
 		camera.rotation.y += ((cameraYRotation + rotationY) - camera.rotation.y) / 20;
 	}
 	
-	/*
+	var camVector = new THREE.Vector3( 0, 0, - 1 );
+	camVector.applyQuaternion( camera.quaternion );
+
 	for (var iter = 0; iter < 1; iter++) {
 		var flake = new letter(console_type, "abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 26)], font);
 		flake.isSnowflake = true;
-		flake.setPosition(Math.random() * 1600, 300, Math.random() * 1600 - 800;
+		flake.setPosition(Math.random() * 1600 - 800, Math.random() * 1000, Math.random() * 1600 - 800);
 		flake.setDestination(flake.position.x + Math.random() * 100 - 50, -200, flake.position.z + Math.random() * 100 - 50);
 		flake.speed = 2;
 
@@ -563,19 +583,19 @@ function render() {
 		scene.add(flake.mesh);
 		consoleLetters.push(flake);
 	}
-	*/
-
+	
+	
 	for (var i = 0; i < consoleLetters.length; i++) {
 		var l = consoleLetters[i];
 		if (l.isDead) {
 			scene.remove(l.mesh);
 			consoleLetters.splice(i, 1);
 		}
-		/*
+		
 		if (l.isSnowflake && l.position.y < -100) {
 			l.free();
 		}
-		*/
+		
 		
 		l.update();
 	}
@@ -606,6 +626,16 @@ function render() {
 			{
 				l.free();
 			}
+		} else if (target.length > 0 && l.text == target[0]) {
+			var t = things[l.thingID];
+			if (t.isBlob) {
+				target.shift();
+				counter++;
+				console.log(target.toString());
+				l.setDestination( - (4 * counter) + (targetStr.length / 2), 0, 75);
+				l.randomFactor = 0;
+				l.sceneArrived = false;
+			}
 		} else if (l.sceneArrived) {
 			var t = things[l.thingID];
 			if (t.isBlob) {
@@ -621,8 +651,17 @@ function render() {
 
 render();
 
-// TODO bind to gyro
 $("body").on("mousemove", function(event) {
-	rotationY = -(event.pageX - window.innerWidth / 2) * 0.001;
-	rotationX = -(event.pageY - window.innerHeight / 2) * 0.001;
+	rotationY = -(event.pageX - window.innerWidth / 2) * 0.005;
+	rotationX = -(event.pageY - window.innerHeight / 2) * 0.005;
 });
+
+$("body").bind("keypress", function(event) {		
+ 	if (event.which == 97) {		
+ 		targetStr = "Phenylpropylaminopentane"
+ 	} else if (event.which >= 98) {		
+ 		targetStr = "Pen Pineapple Apple Pen"
+ 	} else if (event.which >= 99) {		
+ 		targetStr = "Lucy pls"
+ 	}
+ })
